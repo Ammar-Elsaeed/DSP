f3c95a0c75b1f95a3b71760349c277d7ee1c4033
Mostafa Ayad <mostafaayad67@gmail.com>
2021-05-07 02:02:57 +0200

    edit on FFT

--- project3-FFT/Part B/fourier.hpp
+++ project3-FFT/Part B/fourier.hpp
@@ -4,41 +4,36 @@
 using namespace std;
 typedef complex<double> cd;
 typedef long long ll;
-//================================================Cooley-Tukey FFT
-void fft_(vector<cd> &x)
+//================================================Calculate Cooley-Tukey FFT
+vector<cd> fft(vector<cd> &input)
 { // must provide 2^n input size to function properly.
 
 	// Check if it is splitted enough
-	int N = x.size();
+	int N = input.size();
 	if (N <= 1)
-		return;
+		return input;
 
 	// Split even and odd
+	vector<cd> output(N);
 	vector<cd> odd(N / 2);
 	vector<cd> even(N / 2);
 	for (int i = 0; i < N / 2; i++)
 	{
-		even[i] = x[i * 2];
-		odd[i] = x[i * 2 + 1];
+		even[i] = input[i * 2];
+		odd[i] = input[i * 2 + 1];
 	}
 
 	// Split on tasks
-	fft_(even);
-	fft_(odd);
+	even = fft(even);
+	odd = fft(odd);
 
 	// Calculate DFT
 	for (int k = 0; k < N / 2; k++)
 	{
 		cd W = exp(cd(0, -2 * M_PI * k / N));
-		x[k] = even[k] + W * odd[k];
-		x[k + N / 2] = even[k] - W * odd[k];
+		output[k] = even[k] + W * odd[k];
+		output[k + N / 2] = even[k] - W * odd[k];
 	}
-}
-//================================================Calculate FFT
-vector<cd> fft(vector<cd> input)
-{
-	fft_(input);
-	vector<cd> output = input;
 	return output;
 }
 //================================================Calculate FT
